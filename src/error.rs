e857d14dfc6a9aba1ac1223aa65264cbdd157cbc
miRceLzeS <mircelzes727@gmail.com>
2026-05-06 00:35:12 +0800

    feat: basic framework and unified error handling

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub enum ErrorKind {
+    IOError(std::io::Error),
+    CLIErrorr(String),
+    LexError(String),
+    ParseError(String),
+    CogegenError(String),
+}
+
+impl std::fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrorKind::IOError(err) => write!(f, "io error\n{}", err),
+            ErrorKind::CLIErrorr(msg) => write!(f, "cli error\n{}", msg),
+            ErrorKind::LexError(msg) => write!(f, "lex error\n{}", msg),
+            ErrorKind::ParseError(msg) => write!(f, "parse error\n{}", msg),
+            ErrorKind::CogegenError(msg) => write!(f, "code gen error\n{}", msg),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Error(ErrorKind);
+
+impl Error {
+    pub fn new(err_kind: ErrorKind) -> Self {
+        Self(err_kind)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "zerox: {}\n", self.0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::ExitCode;
+
+use zerox::{
+    Result,
+    error::{Error, ErrorKind},
+};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    match run(&args) {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprint!("{}", err);
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run(args: &Vec<String>) -> Result<()> {
+    match args.len() {
+        0 => run_prompt(),
+        1 => run_file(&args[0]),
+        _ => Err(Error::new(ErrorKind::CLIErrorr(
+            "Usage: zerox [file]".into(),
+        ))),
+    }
+}
+
+fn run_prompt<'a>() -> Result<()> {
+    Ok(())
+}
+
+fn run_file<'a>(file_name: &'a str) -> Result<()> {
+    Ok(())
 }
